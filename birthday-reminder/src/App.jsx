9f651b6278e100ff90fd3aae3d16b9d42ef5c111
somberlyx <lybasiddiqui02@gmail.com>
2024-01-17 19:31:41 +0400

    Make 'clear all' functional

--- birthday-reminder/src/App.jsx
+++ birthday-reminder/src/App.jsx
@@ -1,15 +1,24 @@
 import React, { useState } from "react";
 import data from "./store/data";
 import List from "./components/List";
+import NoBirthday from "./components/NoBirthday";
 
 function App() {
   const [people, setPeople] = useState(data);
+
+  const clearAll = () => {
+    setPeople([]);
+  };
   return (
     <main>
       <section className="container">
         <h3>{people.length} birthdays today</h3>
-        <List people={people}></List>
-        <button onClick={() => console.log("you clicked me")}>Clear All</button>
+        {people.length === 0 ? (
+          <NoBirthday />
+        ) : (
+          <List people={people} reset={setPeople} />
+        )}
+        <button onClick={clearAll}>Clear All</button>
       </section>
     </main>
   );
---
+++ birthday-reminder/src/components/NoBirthday.jsx
@@ -0,0 +1,5 @@
+const NoBirthday = () => {
+  return <h2>There's no one to celebrate</h2>;
+};
+
+export default NoBirthday;
