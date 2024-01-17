07dd563add24528941ce71e56d3e93521a042f00
somberlyx <lybasiddiqui02@gmail.com>
2024-01-17 19:22:48 +0400

    Render the list of people

--- birthday-reminder/src/App.jsx
+++ birthday-reminder/src/App.jsx
@@ -3,11 +3,12 @@ import data from "./store/data";
 import List from "./components/List";
 
 function App() {
+  const [people, setPeople] = useState(data);
   return (
     <main>
       <section className="container">
-        <h3>0 birthdays today</h3>
-        <List></List>
+        <h3>{people.length} birthdays today</h3>
+        <List people={people}></List>
         <button onClick={() => console.log("you clicked me")}>Clear All</button>
       </section>
     </main>
