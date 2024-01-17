530b5ccb7f7f57b97ae27d6e876fdfa87aa05aef
somberlyx <lybasiddiqui02@gmail.com>
2024-01-17 19:09:01 +0400

    Create basic ui for birthday reminder

--- birthday-reminder/src/App.jsx
+++ birthday-reminder/src/App.jsx
@@ -1,8 +1,17 @@
 import React, { useState } from "react";
 import data from "./store/data";
 import List from "./components/List";
+
 function App() {
-  return <h2>reminder project setup</h2>;
+  return (
+    <main>
+      <section className="container">
+        <h3>0 birthdays today</h3>
+        <List></List>
+        <button onClick={() => console.log("you clicked me")}>Clear All</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
